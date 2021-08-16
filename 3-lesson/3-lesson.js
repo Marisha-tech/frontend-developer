c88ac154455362b713f9179ea29379c1a79d989c
Карпова <34@3389.ru>
2021-08-16 17:43:28 +0700

    Update

---
+++ 3-lesson/3-lesson.js
@@ -0,0 +1,371 @@
+/* let table = "<table border='1' width='700'>"
+
+let str = 1 //счетчик строк
+
+while(str <= 100) {
+
+  table += "<tr>"
+  let td = 1 //счетчик столбцов
+  while(td <= 10) {
+    table += "<td>" + (str * td) + "</td>"
+    td++
+  }
+  table += "</tr>"
+  str++
+}
+
+table += "</table">
+
+document.write(table) */
+// **************************break - завершает цикл************************************
+/* //Вывести 5 строк, вместо 100
+let table = "<table border='1' width='700'>"
+
+let str = 1 //счетчик строк
+
+while(str <= 100) {
+
+  if(str == 6) {
+    break
+  }
+  
+  table += "<tr>"
+  let td = 1 //счетчик столбцов
+  while(td <= 10) {
+    table += "<td>" + (str * td) + "</td>"
+    td++
+  }
+  table += "</tr>"
+  str++
+}
+table += "</table">
+
+document.write(table) */
+
+// ************************continue - пропускает текущий шаг цикла**************************************
+
+/* //Пропускаем пятый столбец
+let table = "<table border='1' width='700'>"
+
+let str = 1 //счетчик строк
+
+while(str <= 100) {
+
+  if(str == 6) {
+    break
+  }
+  
+  table += "<tr>"
+  let td = 1 //счетчик столбцов
+  while(td <= 10) {
+
+    if(td == 5) {
+      td++
+      continue // не выполняет действие, которое находится ниже, т.е. сразу переходит к условию while(td <= 10)
+    }
+
+    table += "<td>" + (str * td) + "</td>"
+    td++
+  }
+  table += "</tr>"
+  str++
+}
+table += "</table">
+
+document.write(table) */
+// **************************************************************
+/* //Оформление таблицы цветом
+let table = "<table border='1' width='700'>"
+
+let str = 1 //счетчик строк
+let style
+
+while(str <= 100) {
+  
+  table += "<tr>"
+  let td = 1 //счетчик столбцов
+  while(td <= 10) {
+
+    if(td % 2 == 0) {
+      style = "color: red"
+    }
+    else {
+      style = "color: green"
+    }
+
+    table += "<td style='" + style + "'>" + (str * td) + "</td>"
+    td++
+  }
+  table += "</tr>"
+  str++
+}
+table += "</table">
+
+document.write(table) */
+// ***************************Бесконечный цикл***********************************
+/* while (1) {
+  let age = +prompt("Ваш возраст")
+
+  if (!isNaN(age)) {
+    console.log("Введено число")
+    break
+  }
+  else {
+    console.log("Введена строка, введите снова свой ответ")
+  }
+}
+ */
+// **************************************************************
+
+// let i = 0
+// do {
+//   console.log(i)
+//   i++
+// } while(1 < 3)
+
+// *********************Цикл for*****************************************
+//Пример расчета дохода на депозите
+//summa - сумма вклада, year - срок вклада, stavka - ставка вклада, profit - прибыль
+/* function deposit(summa, year) {
+  let stavka = year > 3 ? 6 : 5 //если срок вклада ,больше 3х лет, тогда ставка 6%, иначе 5%
+  let profit
+  let income = summa
+
+  for (let i = 1; i <= year; i++) {
+    profit = (summa * stavka) / 100
+    summa += profit
+    document.write(
+      'За ' +
+        i +
+        ' год Ваша прибыль составит ' +
+        profit.toFixed(2) +
+        ' Ваша сумма составляет ' +
+        summa.toFixed(2) +
+        '<hr>'
+    )
+  }
+
+  let sumIncome = summa - income
+  document.write(
+    'Общий доход за ' + year + ' лет составляет ' + sumIncome.toFixed(2)
+  )
+}
+
+deposit(100000, 5) */
+
+//Пример вывода вклада для каждого рандомного клиента
+/* let money, man, year
+
+for (man = 1; man <= 10; man++) {
+  money = parseInt(Math.random() * 900000 + 100000) //рандомная сумма вклада от 100 000 до 1 000 000
+  year = parseInt(Math.random() * 4 + 1) //рандомный срок вклада от 1 года до 5 лет
+
+  document.write('<h1>Клиент №' + man + ' (сумма = ' + money + ')' + '</h1>')
+  deposit(money, year)
+} */
+
+//Игра угадай число
+/* let money = +prompt('Ваша сумма')
+let stavka = +prompt('Ваша ставка')
+
+if (stavka > money) {
+  console.log('Ставка не может превышать сумму средств')
+} else {
+  let r = (Math.random() * 9 + 1).toFixed(0) //загаданное число от 1 до 10
+  for (let i = 1; i <= 3; i++) {
+    //i - количество попыток
+    let n = prompt('Попытка №' + i)
+
+    if (n == r) {
+      money += stavka
+      alert('Поздравляем! Ваша сумма = ' + money)
+      break
+    } else {
+      if (i == 3) {
+        money -= stavka
+        alert('Вы проиграли! Ваша сумма = ' + money + ' Было загадано ' + r)
+        break
+      }
+    }
+  }
+} */
+
+// ****************************Массивы**********************************
+// let mas = [1, 2, 3, 'str', true, 777]
+// let size = mas.length //длина массива
+// console.log(mas[size - 1]) //последний элемент
+
+//Поиск элемента в массиве
+// console.log(mas.indexOf(777)) //indexOf вернет -1, если элемент не найден в массиве, либо элемент первого вхождения
+
+//Добавление элемента в массив
+// let arr = []
+// arr.push(10, 20)
+// console.log(arr)
+
+//Заполнить массив числами 10 20 30 40 ... 100
+/* for (let i = 0; i < 10; i++) {
+  arr[i] = (i + 1) * 10
+}
+console.log(arr) //выведет в строку [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] */
+
+/* for (let item of arr) {
+  //каждое значение из массива
+  console.log(item) //выведет в столбец http://joxi.ru/12M50eytw8dKO2
+} */
+
+/* for (let i in arr) {
+  //выведет каждый индекс массива
+  console.log(arr[i])
+} */
+
+//Удаление элемента из массива
+/* let deleted = mas.pop()
+mas.pop() //удалит последний элемент из массива
+console.log(mas)
+console.log(deleted) */
+
+//Массив объектов
+/* let goods = [
+  {
+    title: 'Товар 1',
+    price: 100,
+  },
+  {
+    title: 'Товар 2',
+    price: 200,
+  },
+  {
+    title: 'Товар 3',
+    price: 300,
+  },
+]
+
+// for (good of goods) {
+//   console.log(good.title + ' стоит ' + good.price)
+// }
+
+//Сумма всех товаров
+function getSum(mas) {
+  let sum = 0
+
+  for (item of mas) {
+    sum += item.price
+  }
+  return sum
+}
+getSum(goods)
+console.log(getSum(goods)) */
+// ***************************ДЗ***********************************
+// 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100
+//простые чила - которые делятся на себя и на 1 (2, 3, 5, 7, 11, 13, 19)
+/* let number = 20
+let x = 2
+
+function checkPrime(number) {
+  let i = 2
+  while (i < number / 2) {
+    if (number % i == 0) {
+      return false
+    }
+    i++
+  }
+  return true
+}
+
+while (x <= number) {
+  if (checkPrime(x)) {
+    console.log(x)
+  }
+  x++
+}
+ */
+/* let number = 20
+let i = 2
+while (i <= number) {
+  let j = 2
+  while (j <= i) {
+    if (i % j == 0) {
+      break
+    }
+    j++
+  }
+  if (j == i) {
+    console.log(i)
+  }
+  i++
+} */
+// 2. С этого урока начинаем работать с функционалом интернет-магазина. Предположим, есть сущность корзины. Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
+// 3. Товары в корзине хранятся в массиве. Задачи:
+// a) Организовать такой массив для хранения товаров в корзине;
+// b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
+/* let cart = [
+  {
+    title: 'Товар 1',
+    price: 100,
+    quantity: 1,
+  },
+  {
+    title: 'Товар 2',
+    price: 200,
+    quantity: 2,
+  },
+  {
+    title: 'Товар 3',
+    price: 300,
+    quantity: 3,
+  },
+  {
+    title: 'Товар 4',
+    price: 400,
+    quantity: 4,
+  },
+  {
+    title: 'Товар 5',
+    price: 500,
+    quantity: 5,
+  },
+]
+
+function countBasketPrice(mas) {
+  let sum = 0
+  // let quantity = 0
+
+  for (item of mas) {
+    sum += item.price * item.quantity
+    // quantity += item.quantity
+  }
+  // return [sum, quantity]
+  return sum
+}
+
+// countBasketPrice(cart)
+console.log(countBasketPrice(cart)) */
+
+// 4.*Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
+// for(…){// здесь пусто}
+/* let i, one, two
+function numbers(one, two) {
+  for (i = 0; i <= 9; console.log(i++)) {}
+  return i
+}
+numbers(0, 9)
+console.log(numbers(0, 9)) */
+
+// 4) *Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
+// x
+// xx
+// xxx
+// xxxx
+// xxxxx
+// for (let i = 1, line = 'x'; i <= 20; console.log(line), i++, line += 'x') {
+//   console.log()
+// }
+
+/* let x = ''
+for (let i = 0; i < 20; i++) {
+  x += 'x'
+  console.log(x)
+} */
+// **************************************************************
+// **************************************************************
---
+++ 4-lesson/4-lesson.js
@@ -0,0 +1,134 @@
+//join - из массива в строку
+// split - из строки в число
+// indexOf - поиск элемента в массива, те индекс первого вхождения
+// let mas = [1, 2, 3, 4]
+// console.log(typeof mas + ' ' + mas) //object
+// console.log(typeof mas.join('') + ' ' + mas.join('')) //string
+
+// let str = '1234'
+// console.log(typeof str + ' ' + str) //string
+// console.log(typeof str.split(' ') + ' ' + str.split('')) //object
+
+// let s = '1234'
+// let mass = s.split('')
+// console.log(mass[0])
+// console.log(mass.indexOf('4'))
+
+// Тема: объекты в JS
+// Объект - сущность, которая имеет свойства и выполняет действия
+
+/* let car = {
+  name: 'Audi',
+  price: 1000,
+  year: 2020,
+  engineStarted: false,
+  show: function () {
+    console.log(this.name + ' стоит' + this.price)
+  },
+}
+
+let show = function () {
+  console.log(this.name + ' стоит' + this.price)
+}
+
+let car2 = {
+  name: 'Audi',
+  price: 1000,
+  year: 2020,
+  engineStarted: false,
+}
+
+car.color = 'white'
+console.log(car)
+
+car.start = startEngine
+car.start()
+
+car2.start = startEngine
+car2.start()
+
+function startEngine() {
+  if (!this.engineStarted) {
+    this.engineStarted = true
+  }
+}
+
+console.log(car2)
+
+car.stop = function () {
+  if (this.engineStarted) {
+    this.engineStarted = false
+  }
+}
+car.stop()
+console.log(car.engineStarted)
+ */
+
+//Функция конструктор - функция, с помощью которой можно сделать любой объект
+
+/* function Car(marka, price, year) {
+  this.marka = marka
+  this.year = year
+  this.price = price
+  this.show = function () {
+    console.log(this.marka + ' стоит ' + this.price)
+  }
+}
+
+let myCar1 = new Car('Audi', 1000, 2020)
+let myCar2 = new Car('BMW', 2000, 2021)
+let myCar3 = new Car('VW', 1100, 2020) */
+
+// console.log(myCar1)
+// myCar1.show()
+// myCar2.show()
+// myCar3.show()
+
+// let cars = [myCar1, myCar2, myCar3, test]
+// console.log(cars[2].price)
+
+// for (item of cars) {
+//   item.show()
+// }
+
+//Удаление элемента из массива
+// delete myCar2.year
+// console.log(myCar2.year)
+
+//Добавление элемента в массив
+// cars.push(new Car('Vaz', 500, 2012))
+// for (item of cars) {
+//   item.show()
+// }
+
+//Переопределение объекта myCars3 на test
+// let test = myCar3
+// myCar3 = null
+// console.log(cars)
+
+// *********************************ДЗ*********************************
+// 1. Написать функцию, преобразующую число в объект. Передавая на вход число от 0 до 999, мы должны получить на выходе объект, в котором в соответствующих свойствах описаны единицы, десятки и сотни. Например, для числа 245 мы должны получить следующий объект: {‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}. Если число превышает 999, необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
+
+/* let number = prompt('Введите число')
+let num = []
+let obj = {
+  hundreds: '', //сотни
+  dozens: '', //десятки
+  units: '', //единицы
+}
+
+function transformation(number) {
+  if (number <= 999) {
+    num = number.split('')
+    obj.hundreds = num[0]
+    obj.dozens = num[0]
+    obj.units = num[0]
+    console.log(obj)
+  } else {
+    console.log(obj)
+  }
+}
+transformation(number) */
+
+// 2. Для игры, реализованной на уроке, добавить возможность вывода хода номер n (номер задается пользователем)
+// 3. *На базе игры, созданной на уроке, реализовать игру «Кто хочет стать миллионером?»
