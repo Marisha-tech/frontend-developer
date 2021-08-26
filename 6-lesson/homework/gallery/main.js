1bf9c5ef3b2bb0cdff6f7432925bce54b7e95aa2
Карпова <34@3389.ru>
2021-08-26 17:51:43 +0700

    Урок 6: События. ДЗ 1 - галерея изображений

--- 6-lesson/homework/gallery/main.js
+++ 6-lesson/homework/gallery/main.js
@@ -1,7 +1,5 @@
 // 1. Доработать функцию замены картинки в галерее таким образом, чтобы она проверяла наличие картинки по указанному в src адресу.
-// Найти блок с нижней картинкой, которую нужно заменить
-// Найти картинку, по которой кликнули мышкой
-// Заменить картинку
+
 let imagesTableOne = document.querySelectorAll('td') //картинки первой таблицы
 // let imagesTableTwo = document.querySelector('#image-2 td')//td второй таблицы
 let imgTableTwo = document.querySelector('#image-2 td img')//кантинка второй таблицы
@@ -10,16 +8,10 @@ let imgTableTwo = document.querySelector('#image-2 td img')//кантинка в
 for (let i = 0; i < imagesTableOne.length; i++) {
     imagesTableOne[i].onclick = function () {
 
-        // this.classList.toggle('images__allocated--border')
-        // imgTableTwo.id = this.id //добавляем к картинке второй таблицы id выбранной картинки из первой таблицы
-        // imgTableTwo.src = "images/" + this.id + ".jpg"
-        console.log(this.className)
         if (this.className) {
             this.classList.remove('images__allocated--border') //удаляем класс у картинки при втором клике
         } else {
-
             this.classList.add('images__allocated--border')//добавляем картинке класс при первом клике
-            // imagesTableTwo.id = this.id //img-1
             imgTableTwo.id = this.id //добавляем к картинке второй таблицы id выбранной картинки из первой таблицы
             imgTableTwo.src = "images/" + this.id + ".jpg"
         }
